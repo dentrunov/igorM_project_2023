aa32ad29d76bb9aff37543d9258f8293f934adec
dentrunov <sgema@rambler.ru>
2023-12-20 20:02:26 +0300

    ready_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from flask import render_template, request
 
 
@@ -10,6 +10,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap5(app)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -2,19 +2,16 @@ from flask import render_template, request
 from .forms import AuthForm
 from app import app
 
-@app.route('/', methods=["POST", "GET"])
-def hello_world():
-    error = ''
-    form = AuthForm()
-    if form.is_submitted():
-        username = request.form["username"]
-        pasw = request.form["pass"]
-        if username == "111" and pasw == '222':
-            return render_template('success.html', name='Успешная авторизация')
-        else:
-            error = 'Неверные данные'
-    return render_template('index.html', name='Главная страница', error=error, form=form)
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    return render_template('index.html')
 
-@app.route('/second_page')
-def second_page():
+@app.route('/enter')
+def enter():
     return 'second_page'
+
+@app.route('/check')
+def check():
+    return 'third_page'
