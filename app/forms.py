0a27d1adb01c72e7e435e15cbbcf72a0ad20e8ba
dentrunov <sgema@rambler.ru>
2023-12-27 22:50:04 +0300

    fixes docker

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class AuthForm(FlaskForm):
     username = StringField("Имя пользователя")
     pasw = PasswordField("Имя пользователя")
     submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повтори пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Неверное имя пользователя.')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, request
-from .forms import AuthForm
-from app import app
-from flask_login import login_required
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user, login_user
+from urllib.parse import urlsplit
+
+from .forms import AuthForm, RegistrationForm
+from app import app, db
+from .models import User, Pupils
+
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +15,20 @@ def index():
 @app.route('/enter')
 def enter():
     return render_template('enter.html')
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Поздравляю, пользователь зарегистрирован')
+        return redirect(url_for('login'))
+    return render_template('reg.html', title="Регистрация пользователя", form=form)
 
 @app.route('/check')
 @login_required
